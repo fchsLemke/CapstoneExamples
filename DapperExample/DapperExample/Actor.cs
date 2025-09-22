6d5e30798fc1f84c6e219ef917e41aff13ad995a
Mr. Lemke <richard.lemke@ftcsc.org>
2025-09-22 08:56:07 -0400

    Add Controller Example

--- DapperExample/DapperExample/Actor.cs
+++ DapperExample/DapperExample/Actor.cs
@@ -9,7 +9,7 @@ public class Actor
     public int Actor_Id { get; set; }
     public string First_Name { get; set; }
     public string Last_Name { get; set; }
-    public DateTime Last_Update { get; set; }
+    public DateTime? Last_Update { get; set; }
     private int Test { get; set; }
     
 }
